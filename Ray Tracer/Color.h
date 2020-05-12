6e71ec538f1c68e6d2e360b2f6e6f732c7838183
Ömer Köse <omerkose138@gmail.com>
2020-05-12 22:29:07 +0300

    Gamma- Correction has added

--- Ray Tracer/Color.h
+++ Ray Tracer/Color.h
@@ -15,10 +15,11 @@ void write_color(std::ostream &out, Color pixel_color, int samples_per_pixel)
 	auto b = pixel_color.z();
 
 	/*Multiply each channel to get samples' mean to get an average color of pixel*/
+	/*Gamma-corrected for gamma 2.0 meaning that raise color to power of 1/gamma in the case of 2 it is just sqrt*/
 	auto scalar = 1.0 / samples_per_pixel;
-	r *= scalar;
-	g *= scalar;
-	b *= scalar;
+	r = sqrt(scalar * r);
+	g = sqrt(scalar * g);
+	b = sqrt(scalar * b);
 
 
 	//Write the translated [0,255] value of each color component
