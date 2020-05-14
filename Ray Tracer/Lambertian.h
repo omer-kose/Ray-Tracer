fdc3a1b3c10160713e2894bb3808961d77f61ece
Ömer Köse <omerkose138@gmail.com>
2020-05-14 03:18:24 +0300

    Lambertian material has created.

---
+++ Ray Tracer/Lambertian.h
@@ -0,0 +1,28 @@
+#pragma once
+#ifndef LAMBERTIAN_H
+#define LAMBERTIAN_H
+
+#include "Material.h"
+
+class Lambertian : public Material
+{
+public:
+	Lambertian(const Color &alb) : albedo(alb) {}
+
+	virtual bool scatter(const Ray &ray_in, const hit_record &rec, Color &attenuation, Ray &scattered) const
+	{
+		/*Lambertian scatter direction*/
+		/*Offsetting with normal so that we create a unit tangent sphere at the intersection point*/
+		/*Therefore, scattered rays wont go inside of the sphere*/
+		Vec3 scattered_direction = rec.normal + random_unit_vector();
+		/*Creating a new ray with origin at interseciton point*/
+		scattered = Ray(rec.p, scattered_direction);
+		attenuation = albedo;
+		return true;
+	}
+
+public:
+	Color albedo;
+};
+
+#endif
