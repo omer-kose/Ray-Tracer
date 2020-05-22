67d2c0ffc808af3cbc52de62012717a1a03d40b0
Ömer Köse <omerkose138@gmail.com>
2020-05-22 23:17:47 +0300

    Camera Bases has added

--- Ray Tracer/Camera.h
+++ Ray Tracer/Camera.h
@@ -7,29 +7,43 @@
 class Camera
 {
 public:
-	Camera(double vfov, double aspect_ratio)//vfov: vertical field-of-view in degrees
+	Camera
+	(
+		Point3 lookFrom,/*Camera origin in world coordinates*/
+		Point3 lookAt,/*In world coordinates*/
+		Vec3 vup,//This will be used to define an orthonormal basis for camera.(This is also in world coord)
+		double vfov, //vfov: vertical field-of-view in degrees
+		double aspect_ratio
+	)
 	{
+		/*Viewport Definition*/
 		auto theta = degrees_to_radians(vfov);
 		auto h = tan(theta / 2);/*directly tan(theta/2) since we let rays head to z = -1 plane*/
 		auto viewport_height = 2.0 * h;
 		auto viewport_width = aspect_ratio * viewport_height;
 
-		/*Offset of the image plane from the origin*/
-		auto focal_length = 1.0;
+		/*Orthonormal Basis Definition*/
+		auto w = unit_vector(lookFrom - lookAt);
+		auto u = unit_vector(cross(vup,w));
+		auto v = cross(w,u);
 
-		origin = Point3(0.0, 0.0, 0.0);
-		/*Image plane dimensions*/
-		horizontal = Vec3(viewport_width, 0.0, 0.0);
-		vertical = Vec3(0.0, viewport_height, 0.0);
-		/*We are casting our rays relative to lower_left_corner*/
-		lower_left_corner = origin - horizontal / 2 - vertical / 2 - Vec3(0.0, 0.0, focal_length);
-	}
-	/*U and V are parameter which range from 0.0 to 1.0 depending on (pixel/dimension)*/
-	/*u = colIndex/width*/
-	/*v = rowIndex/height*/
-	Ray getRay(double u, double v) const
+
+		/*Image Plane Definiton (Oriented with respect to Camera basis)*/
+		origin = lookFrom;
+		/*Horizontal and Vertical are both used as dimensions and vectors to traverse image plane with scalars range from 0 to 1 (getRay function)*/
+		horizontal = viewport_width * u;/*Points right from left to right along image plane*/
+		vertical = viewport_height * v;/*Points up from bottom to up along image plane*/
+		lower_left_corner = origin - (horizontal / 2) - (vertical / 2) - w;
+
+	} 
+	/*S and T are parameter which range from 0.0 to 1.0 depending on (pixel/dimension)*/
+	/*s = colIndex/width*/
+	/*t = rowIndex/height*/
+	Ray getRay(double s, double t) const
 	{
-		return Ray(origin, lower_left_corner + u * horizontal + v * vertical);
+		/*Since origin is not (0,0,0) anymore to get true direction we have to subtract origin(cameraOrigin)*/
+		/*Like any other direction determination calculations*/
+		return Ray(origin, lower_left_corner + s * horizontal + t * vertical - origin);
 	}
 
 private:
